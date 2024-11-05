8921c508c7324c17c09e1765b8ce1b2acfcc382a
letcla0624 <letcla0624@yahoo.com.tw>
2024-11-05 19:36:22 +0800

    feat: 使用 Bootstrap 樣式

--- pages/index.vue
+++ pages/index.vue
@@ -1,10 +1,26 @@
-<script setup lang="ts">
-const route = useRoute();
-</script>
+<script lang="ts" setup></script>
 
 <template>
   <div>
-    <h1>Page: 首頁</h1>
-    <p>目前路由的路徑 - route.fullPath 為：{{ route.fullPath }}</p>
+    <p class="text">Page: index</p>
+    <div>
+      <button type="button" class="btn btn-primary">Primary</button>
+      <button type="button" class="btn btn-secondary">Secondary</button>
+      <button type="button" class="btn btn-success">Success</button>
+      <button type="button" class="btn btn-danger">Danger</button>
+      <button type="button" class="btn btn-warning">Warning</button>
+      <button type="button" class="btn btn-info">Info</button>
+      <button type="button" class="btn btn-light">Light</button>
+      <button type="button" class="btn btn-dark">Dark</button>
+
+      <button type="button" class="btn btn-link">Link</button>
+    </div>
   </div>
 </template>
+
+<style lang="scss" scoped>
+.text {
+  color: $primary;
+  font-size: $fs-xl;
+}
+</style>
